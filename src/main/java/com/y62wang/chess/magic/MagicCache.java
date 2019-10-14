424c9a33a870d4efb579088911c874ba1972c419
Yang Wang <y62wang@gmail.com>
2019-10-14 00:46:00 -0700

    reduce duplication

--- src/main/java/com/y62wang/chess/magic/MagicCache.java
+++ src/main/java/com/y62wang/chess/magic/MagicCache.java
@@ -3,15 +3,16 @@ package com.y62wang.chess.magic;
 import com.y62wang.chess.Bishop;
 import com.y62wang.chess.BoardUtil;
 import com.y62wang.chess.Rook;
-import com.y62wang.chess.Util;
 
 import java.util.List;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 
-import static com.y62wang.chess.BoardConstants.BB_FILE_D;
 import static com.y62wang.chess.BoardConstants.BOARD_SIZE;
-import static com.y62wang.chess.magic.MagicConstants.*;
+import static com.y62wang.chess.magic.MagicConstants.BISHOP_MAGIC;
+import static com.y62wang.chess.magic.MagicConstants.BISHOP_MAGIC_BITS;
+import static com.y62wang.chess.magic.MagicConstants.ROOK_MAGIC;
+import static com.y62wang.chess.magic.MagicConstants.ROOK_MAGIC_BITS;
 
 public class MagicCache
 {
@@ -24,20 +25,7 @@ public class MagicCache
         bishopMagic = prepareMagic(BISHOP_MAGIC, BISHOP_MAGIC_BITS, Bishop::bishopMask, Bishop::bishopAttacks);
     }
 
-    private void prepareBishopMagic()
-    {
-        for (int square = 0; square < BOARD_SIZE; square++)
-        {
-            int bitShifts = BOARD_SIZE - BISHOP_MAGIC_BITS[square];
-            long magicNumber = BISHOP_MAGIC[square];
-            long mask = Bishop.bishopMask(square);
-            long[] attacks = indexBishopAttacksCache(square, bitShifts, magicNumber, mask);
-            Magic magic = new Magic(bitShifts, magicNumber, mask, attacks);
-            this.bishopMagic[square] = magic;
-        }
-    }
-
-    private Magic[] prepareMagic(long[] magicNumbers, int[] magicBits, Function<Integer, Long> maskFunction, BiFunction<Integer, Long, Long> attackSetFunction)
+    private Magic[] prepareMagic(long[] magicNumbers, int[] magicBits, Function<Integer, Long> maskFunction, BiFunction<Integer, Long, Long> attackFunction)
     {
         Magic[] magicCache = new Magic[BOARD_SIZE];
         for (int square = 0; square < BOARD_SIZE; square++)
@@ -45,64 +33,22 @@ public class MagicCache
             int bitShifts = BOARD_SIZE - magicBits[square];
             long magicNumber = magicNumbers[square];
             long mask = maskFunction.apply(square);
-            long[] attacks = indexAttacksCache(square, magicBits[square], magicNumber, mask, attackSetFunction);
+            long[] attacks = indexAttacksCache(square, magicBits[square], magicNumber, mask, attackFunction);
             Magic magic = new Magic(bitShifts, magicNumber, mask, attacks);
             magicCache[square] = magic;
         }
         return magicCache;
     }
 
-    private long[] indexAttacksCache(final int square, final int magicBits, final long magicNumber, final long mask, BiFunction<Integer, Long, Long> attackSetFunction)
+    private long[] indexAttacksCache(final int square, final int magicBits, final long magicNumber, final long mask, BiFunction<Integer, Long, Long> attackFunction)
     {
         List<Integer> squares = BoardUtil.squaresOfBB(mask);
         long[] attacks = new long[( int ) Math.pow(2, magicBits)];
-        for (int j = 0; j < ( int ) Math.pow(2, squares.size()); j++)
+        for (int encodedCombination = 0; encodedCombination < ( int ) Math.pow(2, squares.size()); encodedCombination++)
         {
-            long occupancy = getOccupiedSquares(squares, j);
-            long attackSet = attackSetFunction.apply(square, occupancy);
+            long occupancy = getOccupiedSquares(squares, encodedCombination);
+            long attackSet = attackFunction.apply(square, occupancy);
             int key = ( int ) ((occupancy * magicNumber) >>> (64 - magicBits));
-            attacks[key] = attackSet;
-            System.out.println("Key " + key);
-        }
-        return attacks;
-    }
-
-    private void prepareRookMagic()
-    {
-        for (int square = 0; square < BOARD_SIZE; square++)
-        {
-            int bitShifts = BOARD_SIZE - ROOK_MAGIC_BITS[square];
-            long magicNumber = ROOK_MAGIC[square];
-            long mask = Rook.rookMask(square);
-            long[] attacks = indexRookAttacksCache(square, bitShifts, magicNumber, mask);
-            Magic magic = new Magic(bitShifts, magicNumber, mask, attacks);
-            this.rookMagic[square] = magic;
-        }
-    }
-
-    private long[] indexRookAttacksCache(final int square, final int bitShifts, final long magicNumber, final long mask)
-    {
-        List<Integer> squares = BoardUtil.squaresOfBB(mask);
-        long[] attacks = new long[( int ) Math.pow(2, ROOK_MAGIC_BITS[square])];
-        for (int j = 0; j < ( int ) Math.pow(2, squares.size()); j++)
-        {
-            long occupancy = getOccupiedSquares(squares, j);
-            long attackSet = Rook.rookAttacks(square, occupancy);
-            int key = ( int ) ((occupancy * magicNumber) >>> bitShifts);
-            attacks[key] = attackSet;
-        }
-        return attacks;
-    }
-
-    private long[] indexBishopAttacksCache(final int square, final int bitShifts, final long magicNumber, final long mask)
-    {
-        List<Integer> squares = BoardUtil.squaresOfBB(mask);
-        long[] attacks = new long[( int ) Math.pow(2, BISHOP_MAGIC_BITS[square])];
-        for (int j = 0; j < ( int ) Math.pow(2, squares.size()); j++)
-        {
-            long occupancy = getOccupiedSquares(squares, j);
-            long attackSet = Bishop.bishopAttacks(square, occupancy);
-            int key = ( int ) ((occupancy * magicNumber) >>> bitShifts);
             attacks[key] = attackSet;
         }
         return attacks;
