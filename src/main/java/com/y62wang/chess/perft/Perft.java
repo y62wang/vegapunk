4240d79e885426b13b20445f0429318bba23dcbf
Yang Wang <y62wang@gmail.com>
2019-10-23 21:09:51 -0700

    improve perft

--- src/main/java/com/y62wang/chess/perft/Perft.java
+++ src/main/java/com/y62wang/chess/perft/Perft.java
@@ -2,35 +2,24 @@ package com.y62wang.chess.perft;
 
 import com.google.common.base.Stopwatch;
 import com.y62wang.chess.Bitboard;
-import com.y62wang.chess.Move;
 import org.junit.Assert;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class Perft
 {
-    public static void singlePerftNonTest(Bitboard board, int depth, long expectedNodeCount)
-    {
-        Map<Integer, Long> map = new HashMap<>();
-        Map<String, Integer> roots = new HashMap<>();
-        perftTime(board, depth);
-        roots.forEach((key, val) -> System.out.println(String.format("%s %s", key, val)));
-    }
-
     public static void singlePerft(Bitboard board, int depth, long expectedNodeCount)
     {
         Map<Integer, Long> map = new HashMap<>();
         Map<String, Integer> roots = new HashMap<>();
-        long result = perftTime(board, depth);
+        long result = timedPerft(board, depth);
         System.out.println(roots.keySet() + " " + roots.values());
         System.out.println("==============================================================");
-        // roots.forEach((key, val) -> System.out.println(String.format("%s %s", key, val)));
         Assert.assertEquals(Long.valueOf(expectedNodeCount), Long.valueOf(result));
     }
 
-    public static void perft(Bitboard startingBoard, long[] expectedNodeCountByDepth)
+    public static void validatedPerfts(Bitboard startingBoard, long[] expectedNodeCountByDepth)
     {
         for (int index = 0; index < expectedNodeCountByDepth.length; index++)
         {
@@ -38,68 +27,29 @@ public class Perft
         }
     }
 
-    public static long perftTime(Bitboard board, int depth)
+    public static long timedPerft(Bitboard board, int depth)
     {
         Stopwatch stopwatch = Stopwatch.createStarted();
-        long result = perftPure(board, depth, 0);
+        long result = perft(board, depth);
         stopwatch.stop();
         long millis = stopwatch.elapsed().toMillis();
         System.out.println(String.format("perft(%s) %6s ms %10s nodes %7s nodes/ms", depth, millis, result, millis == 0 ? 0 : result / millis));
         return result;
     }
 
-    private static long perftPure(Bitboard startingBoard, int targetDepth, int currentDepth)
+    private static long perft(Bitboard startingBoard, int depth)
     {
-        if (currentDepth == targetDepth)
+        if (depth==1)
         {
-            return 1;
+            return startingBoard.legalMoves().size();
         }
+
         long result = 0;
         for (short possibleMove : startingBoard.legalMoves())
         {
             Bitboard board = startingBoard.makeMove(possibleMove);
-            if (currentDepth == 0)
-            {
-                board.rootMove = possibleMove;
-            }
-            else
-            {
-                board.rootMove = startingBoard.rootMove;
-            }
-            result = result + perftPure(board, targetDepth, currentDepth + 1);
+            result = result + perft(board, depth - 1);
         }
         return result;
-    }
-
-    private static void perft(Bitboard startingBoard, int targetDepth, int currentDepth, Map<Integer, Long> nodes, Map<String, Integer> roots, short lastMove)
-    {
-        if (currentDepth == targetDepth)
-        {
-            nodes.putIfAbsent(targetDepth, 0L);
-            nodes.put(targetDepth, nodes.get(targetDepth) + 1);
-            String key = Move.moveString(startingBoard.rootMove);
-            // String key = startingBoard.rootMove.toString() + "-" + startingBoard.rootMove.getFlag();
-            roots.putIfAbsent(key, 0);
-            roots.put(key, roots.get(key) + 1);
-            return;
-        }
-        List<Short> legalMoves = startingBoard.legalMoves();
-//        String repeated = new String(new char[currentDepth]).replace("\0", "    ");
-//        System.out.print(repeated + "> " + legalMoves.size() + " ");
-//        Util.printMoves(legalMoves);
-        for (short possibleMove : legalMoves)
-        {
-//            System.out.println(repeated + " Making move " + Move.moveString(possibleMove));
-            Bitboard board = startingBoard.makeMove(possibleMove);
-            if (currentDepth == 0)
-            {
-                board.rootMove = possibleMove;
-            }
-            else
-            {
-                board.rootMove = startingBoard.rootMove;
-            }
-            perft(board, targetDepth, currentDepth + 1, nodes, roots, possibleMove);
-        }
     }
 }
--- src/main/java/com/y62wang/chess/programs/PerftDebugger.java
+++ src/main/java/com/y62wang/chess/programs/PerftDebugger.java
@@ -7,18 +7,19 @@ public class PerftDebugger
 {
     public static void main(String[] args)
     {
-        Bitboard board = new Bitboard(args[0]);
         int depth = Integer.parseInt(args[1]);
         int runs = 1;
         if (args.length == 3)
         {
             runs = Integer.parseInt(args[2]);
         }
+
         for (int i = 0; i < runs; i++)
         {
+            Bitboard board = new Bitboard(args[0]);
             Bitboard.MAKE_MOVE_TIME = 0;
             Bitboard.LEGAL_MOVE_TIME = 0;
-            Perft.singlePerftNonTest(board, depth, 0);
+            Perft.timedPerft(board, depth);
 
             if (Bitboard.MAKE_MOVE_TIME != 0 || Bitboard.LEGAL_MOVE_TIME != 0)
             {
